0bce7d58704692612a3b703d737f80a4f9470db2
Christian Liebel <christian.liebel@thinktecture.com>
2018-10-11 11:16:16 +0200

    Configuring routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { AboutComponent } from './about/about.component';
+import { SpeechComponent } from './speech/speech.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'speech', component: SpeechComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
