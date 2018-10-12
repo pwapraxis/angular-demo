c7433b7b44a7fae3155b4fa094b2b643f83c16bf
Christian Liebel <christian.liebel@thinktecture.com>
2018-10-12 21:53:33 +0200

    Renaming TodoServcie in test spec

--- src/app/database.service.spec.ts
+++ src/app/database.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TodoService } from './todo.service';
+import { DatabaseService } from './database.service';
 
-describe('TodoService', () => {
+describe('DatabaseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
+    const service: DatabaseService = TestBed.get(DatabaseService);
     expect(service).toBeTruthy();
   });
 });
