000af7ac9770ad6addb83e951ed84beceb57a78e
Christian Liebel <christian.liebel@thinktecture.com>
2018-10-15 19:28:03 +0200

    Install behaviour

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, HostListener } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SyncService } from '../sync.service';
@@ -10,12 +10,29 @@ import { SyncService } from '../sync.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  installPrompt;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
   constructor(private breakpointObserver: BreakpointObserver, private syncService: SyncService) {}
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event) {
+    this.installPrompt = event;
+    return false;
+  }
+
+  @HostListener('window:appinstalled')
+  onAppInstalled() {
+    this.installPrompt = null;
+  }
+
+  install() {
+    this.installPrompt.prompt();
+  }
 
   sync() {
     this.syncService.sync();
