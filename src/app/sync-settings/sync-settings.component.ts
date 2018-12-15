4414228446e500bf73e858f22308b69a036a3ca7
Christian Liebel <christian.liebel@thinktecture.com>
2018-12-15 18:13:40 +0100

    Add component for sync settings

---
+++ src/app/sync-settings/sync-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sync-settings',
+  templateUrl: './sync-settings.component.html',
+  styleUrls: ['./sync-settings.component.css']
+})
+export class SyncSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
